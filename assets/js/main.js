e9d086dee9c82a03d57028fe1b3097f1176eb707
Melanie Casasco <melaniecasasco@gmail.com>
2021-06-22 15:08:29 -0300

    cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,13 +35,23 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
-sr.reveal('.home__subtitle',{delay: 200}); 
 sr.reveal('.button',{delay: 200}); 
 sr.reveal('.home__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 
-/*SCROLL quienesSomos*/
-sr.reveal('.quienesSomos__img',{}); 
-sr.reveal('.quienesSomos__subtitle',{delay: 400}); 
-sr.reveal('.quienesSomos__text',{delay: 400}); 
+/*SCROLL ABOUT*/
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
 
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle',{}); 
+sr.reveal('.skills__text',{}); 
+sr.reveal('.skills__data',{interval: 200}); 
+sr.reveal('.skills__img',{delay: 600});
+
+/*SCROLL WORK*/
+sr.reveal('.work__img',{interval: 200}); 
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input',{interval: 200}); 
